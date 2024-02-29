2df67c08b5145d2b5ee76c76c5743cd27317af03
pmugabo <p.mugabo@alustudent.com>
2024-02-29 06:26:05 -0800

    last digit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number)
+    if number < 0:
+        number = -number
+    las_ digit = number % 10
+    #print("{}".format(last_digitt), end='')
+    return(last_digit)
