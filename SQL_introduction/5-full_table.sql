8c629d8c7bccbeaa1a70f03ee18bc862eb59feae
pmugabo <p.mugabo@alustudent.com>
2024-03-23 07:31:53 -0700

    list tables

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
---Prints the full decription of the table first_table in my MySQL server.
+-- Prints the full decription of the table first_table in my MySQL server.
 SHOW CREATE TABLE `first_table`;
