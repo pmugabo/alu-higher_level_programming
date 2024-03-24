4b0d08aefd4787b2988e5aef9631593058ebbff3
pmugabo <p.mugabo@alustudent.com>
2024-03-24 14:40:34 -0700

    permissions

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX GET $1 -L
+# Sends a GET request to a URL, and displays the body of the response
+curl -sL "$1"
